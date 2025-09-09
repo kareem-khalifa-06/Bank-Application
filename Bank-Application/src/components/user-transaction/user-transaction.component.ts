6337a9a3567de36f780e735f66735fed468ad505
Kinoo <karemkhalifaa@gmail.com>
2025-09-09 09:56:16 +0300

    Adding needed components

---
+++ Bank-Application/src/components/user-transaction/user-transaction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-transaction',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-transaction.component.html',
+  styleUrl: './user-transaction.component.css'
+})
+export class UserTransactionComponent {
+
+}
---
+++ Bank-Application/src/components/user-transfer/user-transfer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-transfer',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-transfer.component.html',
+  styleUrl: './user-transfer.component.css'
+})
+export class UserTransferComponent {
+
+}
